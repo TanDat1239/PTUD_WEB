f2605afedb54ff6af157b9009bdd7b2d0416c01b
Tan Dat <datdiep01@gmail.com>
2022-03-05 10:06:03 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { BadRequestError, errorHandler } = require("./app/errors");
+const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
+});
+
+setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res);
 });
 
 module.exports = app;
